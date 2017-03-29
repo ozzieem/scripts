30d00639f491c648e734320c2a96a1dd96a2f460
ozzieem <ozzieee@gmail.com>
2017-03-29 22:41:40 +0200

    init

---
+++ jscript.js
@@ -0,0 +1,7 @@
+function openSYT() {
+	document.getElementById("center").innerHTML='<object type="text/html" data="scripts/searchYT.html" width=100% height=100%></object>';
+}
+
+function openOML() {
+	document.getElementById("center").innerHTML='<object type="text/html" data="scripts/searchWeb.html" width=100% height=100%></object>';	
+}
